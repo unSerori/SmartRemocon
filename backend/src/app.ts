bfbbca55b081dd0e9a51d46696159f6d14e029f6
unSerori <pd.ai.374005475@gmail.com>
2025-06-03 13:07:02 +0900

    [setup]utility/setup/node_ts.mdの作成を行い、その過程で開発環境（eslint, prettier, tsx）のカスタマイズを行った

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,25 @@
 /**
  * サーバー起動スクリプト
- * 
+ *
  * このスクリプトは、Expressを使用して基本的なHTTPサーバーを起動します。
  * JSONリクエストボディの解析ミドルウェアを設定し、指定されたポートで待ち受けます。
- * 
+ *
  * 使用技術:
  * - Express（Webフレームワーク）
  * - Node.js httpモジュール
- * 
+ *
  *  Date : 2025/04/01 - 2025/07/31
- *  Author : K.Murakami
+ *  Author :
  */
-
 
 // Expressライブラリをインポート（Node.jsのWebアプリケーションフレームワーク）
 import express from 'express';
 
 // httpモジュールをインポート（Node.jsの標準モジュール、HTTPサーバーを作成するために使用）
 import http from 'http';
+import { routing1 } from './route/router1.js';
+
+// import
 
 // expressアプリケーションのインスタンスを作成
 const app = express();
@@ -26,12 +28,18 @@ const app = express();
 const server = http.createServer(app);
 
 // 受信するリクエストのボディをJSONとして自動的に解析するミドルウェアを追加
-app.use(express.json());
+// app.use(express.json());
 
 // サーバーがリッスンするポート番号を指定
 const port = 8000;
 
+console.log('aaaa');
+
+routing1(3);
+
 // 指定したポートでHTTPサーバーを起動し、起動成功時にメッセージを出力
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// websocket
---
+++ backend/src/route/router1.ts
@@ -0,0 +1,4 @@
+export function routing1(params: number) {
+  console.log(params);
+  console.log('aa');
+}

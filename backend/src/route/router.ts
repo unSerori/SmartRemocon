b486810345584483d093943cf01221dca1ae1bff
unSerori <pd.ai.374005475@gmail.com>
2025-06-10 11:40:23 +0900

    [update]2完了
    httpで受け取ったサンプルデータをインメモリ配列に追加してアクセス時に返す

--- backend/src/route/router.ts
+++ backend/src/route/router.ts
@@ -1,5 +1,18 @@
 import express from 'express'; // Expressライブラリをインポート（Node.jsのWebアプリケーションフレームワーク）
 
+type EnvLog = {
+  id: number;
+  // device: Device;
+  temperatureSht: number;
+  humidity: number;
+  temperatureQmp: number;
+  pressure: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+const testDataList: EnvLog[] = [];
+
 export function routing(app: express.Express) {
   app.get('/test', async (req, res) => {
     // ex: http://localhost:8000/test?key=value
@@ -13,12 +26,27 @@ export function routing(app: express.Express) {
   // TODO: 将来的にはMQTTにするが、一旦HTTPで想定される値を受け取るようにする
   app.post('/env', async (req, res) => {
     console.log(req.body);
-    res.status(200).json('');
+    console.log(testDataList);
+
+    const testData: EnvLog = {
+      id: testDataList.length,
+      // device: Device;
+      temperatureSht: req.body['temperature_sht'],
+      humidity: req.body['humidity'],
+      temperatureQmp: req.body['temperature_qmp'],
+      pressure: req.body['pressure'],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    testDataList.push(testData);
+    res.status(200).json(testDataList);
   });
 
   app.get('/env-logs', async (req, res) => {
-    console.log('/env-logs ok?');
+    console.log('/env-logs ok!');
+    console.log('testData: ', testDataList); // console.log(`testData: ${testDataList}`);
 
-    res.json(req.query);
+    res.json(testDataList);
   });
 }

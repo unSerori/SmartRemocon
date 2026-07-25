0ab5a181bfe65aec73698e8b444f8f068a161a70
unSerori <pd.ai.374005475@gmail.com>
2026-07-25 17:03:06 +0000

    feat: prisma導入およびテーブルスキーマ定義・生成

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { EnvLogRepo } from './env-log.repository';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, EnvLogRepo],
 })
 export class AppModule {}

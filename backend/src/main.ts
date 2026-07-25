e8c2f1ba7e176af04d98d78ab0b8d496e8d60614
unSerori <pd.ai.374005475@gmail.com>
2026-07-25 17:09:40 +0000

    fix: コンパイルエラー・型エラー・リントエラー修正

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000); // Cannot find name 'process'. Do you need to install type definitions for node? Try `npm i --save-dev @types/node` and then add 'node' to the types field in your tsconfig.
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});

9ed455b39b88c7cec01f4c287d7c2a037f5fbc6d
unSerori <pd.ai.374005475@gmail.com>
2026-07-27 06:19:41 +0000

    feat: srcも移行

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { PostEnvDTO } from './dto/req.js';
 import { AppService } from './app.service.js';
+import { EnvLogResDto } from './dto/res/env-log.js';
 
+// Request DTO
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +11,32 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  getTest(): string {
+    return this.appService.getTest();
+  }
+
+  // TODO: `postEnvLog`に直す
+  @Post('env')
+  async recordEnvLog(@Body() body: PostEnvDTO) {
+    console.log('body.temperatureSht: ', body.temperatureSht);
+
+    const savedData = await this.appService.createEnvLog(body);
+
+    return {
+      data: savedData,
+    };
+  }
+
+  @Get('env-logs')
+  async getEnvLogs(@Query('limit') limit?: string) {
+    console.log('/env-logs controller!');
+
+    const limitNum = limit ? Number(limit) : undefined;
+    const envLogs = await this.appService.fetchEnvLogs(limitNum); // 将来的には、srvから得た成果物を、HTTPレスポンスの形としてDTOをかます
+
+    return EnvLogResDto.fromEntities(envLogs);
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaService } from './prisma.service.js';
+import { EnvLogRepo } from './env-log.repository.js';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, EnvLogRepo],
 })
 export class AppModule {}

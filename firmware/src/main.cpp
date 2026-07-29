283ed6b8b573d0fa91d5a0f2e4287ba65d7514ac
unSerori <pd.ai.374005475@gmail.com>
2026-07-30 00:28:55 +0900

    feat: M5のセットアップとボタン確認

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,21 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <M5Unified.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  auto cfg = M5.config();
+  M5.begin(cfg);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  M5.update();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (M5.BtnC.wasPressed())
+  {
+    auto msg = "Pressed Btn C.";
+    Serial.println(msg);
+    M5.Display.clear();
+    M5.Display.setTextSize(2);
+    M5.Display.setTextColor(WHITE); // 第二引数には背景色も入れれる（デフォは等価）
+    M5.Display.setCursor(0, 0);
+    M5.Display.println(msg);
+  }
 }

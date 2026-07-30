f07c31992fec9aa0c5cdc37a5b0972e2c1d57419
unSerori <pd.ai.374005475@gmail.com>
2026-07-30 16:11:02 +0900

    feat: 受送信

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -6,6 +6,7 @@
 #include <M5Unified.h>
 #include <ESPmDNS.h>
 #include <WiFi.h> // あとで消す
+#include <IRremote.hpp> // hpp
 #include "credentials.h"
 #include "sensor/EnvSensor.h"
 #include "network/HttpEnvSender.h"
@@ -13,16 +14,19 @@
 #include "usecase/SendEnvDataUseCase.h"
 
 // ピン番号をマクロで定義
-//
+#define IR_RECEIVE_PIN 36 // 受信: 36番ピン
+#define IR_SEND_PIN 26 // 送信: 26番ピン
 
 // 変数宣言
-//
+IRData last_ir_data;
 
 // オブジェクト作成
 EnvSensor env_sensor;
 WiFiConnector wifi_connector;
 HttpEnvSender env_sender("http", HOST, 8080, "/api/env", wifi_connector);
 SendEnvDataUseCase send_env_data_use_case(env_sensor, env_sender);
+
+// TODO: とりあえず送受信をマイコンで完結させるために変数定義すべきか？
 
 void setup() {
   Serial.begin(115200);
@@ -50,13 +54,57 @@ void setup() {
     delay(1000);
   }
   Serial.println("mDNS started!");
-
   Serial.printf("WiFi.localIP(): %s\n", WiFi.localIP().toString().c_str());
+
+  IrSender.begin(IR_SEND_PIN);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); 
 }
 
 void loop() {
   M5.update();
 
+  // Aを押すと取得
+  if (IrReceiver.decode()) { 
+    if (M5.BtnA.isPressed()){
+      if (IrReceiver.decodedIRData.decodedRawData != 0) // HACK: これは多分ブス
+      {
+        last_ir_data = IrReceiver.decodedIRData;
+        Serial.println(last_ir_data.decodedRawData, HEX);
+
+        IrReceiver.printIRResultShort(&Serial);  // 受信したデータの簡潔な概要を表示
+        IrReceiver.printIRSendUsage(&Serial);    // 受信した信号を送信するためのコードを表示
+      }
+    }
+    IrReceiver.resume();
+  }
+
+  // // B押したら送信
+  // if (M5.BtnB.wasPressed())
+  // {
+  //   Serial.println("IR Send: Start.");
+  //   IrSender.sendNEC(last_ir_data.address, last_ir_data.command, 0);
+  //   Serial.println("IR Send: End.");
+  // }
+  // B押したら送信
+  if (M5.BtnB.wasPressed())
+  {
+    Serial.println("IR Send: Start.");
+    // IrSender.sendNEC(last_ir_data.address, last_ir_data.command, 0);
+    IrSender.sendPulseDistanceWidth(
+      38,                     // 赤外線搬送波周波数（kHz）
+      9000, 4500,             // リーダーパルス（ON, OFF）
+      560, 1690,              // "1" のパルス幅（ON, OFF）
+      560, 550,               // "0" のパルス幅（ON, OFF）
+      last_ir_data.decodedRawData,          // 送信データ（48ビット）
+      48,                     // データ長（ビット数）
+      PROTOCOL_IS_LSB_FIRST,  // LSBファースト
+      0,                      // 繰り返し周期（0 = 送信しない）
+      2                       // 送信回数（リモコンのように2回送る）
+  );
+    Serial.println("IR Send: End.");
+  }
+
+  // C押したら環境値送信
   if (M5.BtnC.wasPressed())
   {
     send_env_data_use_case.execute();
